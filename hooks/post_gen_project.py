a50f031b767de1fd9f59bc567646ab1f57289935
Lucel Da Silva <luceldasilva@yahoo.com>
2024-05-21 23:34:16 -0400

    🐛 Mostrar mensaje de cd y la ruta del proyecto
    eso se muestra con os igual que esta en pre_gen_project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,9 @@
-from pre_gen_project import project_slug
+import os
 import subprocess
 
 
 MESSAGE_COLOR = "\x1b[44m"
-YML_COLOR = "\x1b[38;5;190m	"
+YML_COLOR = "\x1b[38;5;190m"
 OS_COLOR = "\x1b[42m"
 INFO_COLOR = "\x1b[38;5;219m"
 RESET_ALL = "\x1b[0m"
@@ -18,7 +18,7 @@ subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 subprocess.call(['git', 'tag', "{{ cookiecutter.project_version }}"])
 
 print(f"{INFO_COLOR}Please enter the project folder with this command.{RESET_ALL}")
-print(f"{OS_COLOR}cd {project_slug}{RESET_ALL}")
+print(f"{OS_COLOR}cd { os.getcwd() }{{RESET_ALL}")
 print(f"{INFO_COLOR}And then, copy and paste into the terminal to install the dependencies. If you prefer, you can use mamba instead of conda.{RESET_ALL}")
 print(f"{YML_COLOR}conda env create --file environment.yml{RESET_ALL}")
 print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
